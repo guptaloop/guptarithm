6f67f69419b2e31203b189a568769aca30589c38
Abhi Gupta <abhifgupta@gmail.com>
2019-08-30 18:41:32 -0700

    merge repo, delete frontend

--- frontend/src/reducers/root_reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import session from './session/session_reducer';
-
-const RootReducer = combineReducers({
-	session,
-	errors
-});
-
-export default RootReducer;
