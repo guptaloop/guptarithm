c602501e0cb06c2cad9877f62bef38a0c88682b0
Abhi Gupta <abhifgupta@gmail.com>
2019-08-31 00:30:50 -0700

    set up frontend auth and verify react components render in localhost

---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import session from './session/session_reducer';
+import errors from './errors/errors_reducer';
+
+const RootReducer = combineReducers({
+	session,
+	errors,
+	
+});
+
+export default RootReducer;
