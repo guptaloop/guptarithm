479377ed23686bf4b3284f2dd46f28cba92b0933
Abhi Gupta <abhifgupta@gmail.com>
2019-09-30 13:55:41 -0700

    create new Allocation schema, remove allocation hash from Holding

--- models/Holding.js
+++ models/Holding.js
@@ -4,22 +4,13 @@ const Schema = mongoose.Schema;
 const HoldingSchema = new Schema({
 	user: { type: Schema.Types.ObjectId, ref: 'users', required: true	},
 	account: { type: Schema.Types.ObjectId,	ref: 'accounts', required: true	},
+	allocation: { type: Schema.Types.ObjectId,	ref: 'allocation', required: true },
 	symbol: {	type: String,	required: true,	minlength: 1,	maxlength: 5 },
 	name: { type: String,	required: true, minlength: 3, maxlength: 50 },
 	type: { enum: ['ETF', 'Mutual Fund', 'Stock', 'Other'], 
 					type: String,	required: true },
 	exp_ratio: { type: Number, required: true	},
 	shares: {	type: Number,	required: true },
-	// should there be a separate 'allocation' schema so that you don't have to duplicate the allocation info for each security?
-	allocation: {
-		usStocks: { type: Number, default: 0, min: 0, max: 100 },
-		forStocks: { type: Number, default: 0, min: 0, max: 100 },
-		eM: { type: Number, default: 0, min: 0, max: 100 },
-		smallCap: { type: Number, default: 0, min: 0, max: 100 },
-		indStocks: { type: Number, default: 0, min: 0, max: 100 },
-		bonds: { type: Number, default: 0, min: 0, max: 100 },
-		other: { type: Number, default: 0, min: 0, max: 100 }
-	},
 	date: { type: Date,	default: Date.now	}
 });
 
