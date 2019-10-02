788a72cec8a0712a1879e15e71c102fb763915c8
Abhi Gupta <abhifgupta@gmail.com>
2019-10-02 00:10:26 -0700

    update app name to Anubis

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -8,8 +8,8 @@ class Splash extends React.Component {
 				<div className="hero-banner">
 				</div>
 				<div className="hero-image">
-					<p>Welcome to Madhu</p>
-					<h1>The free investment algorithm</h1>
+					<p>Welcome to Anubis</p>
+					{/* <h1>The free investment algorithm</h1> */}
 				</div>
 			</>
 		);
