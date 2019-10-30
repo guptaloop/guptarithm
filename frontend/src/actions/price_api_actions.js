dbe8d4101e240feb3675b0147f80ebacee47a4ef
Abhi Gupta <abhifgupta@gmail.com>
2019-10-30 15:10:55 -0700

    Revert "Revert "update state to get prices""
    This reverts commit d0a1389aab328d27e4382653c86f46532b997ed0.

--- frontend/src/actions/price_api_actions.js
+++ frontend/src/actions/price_api_actions.js
@@ -7,8 +7,15 @@ export const receivePrice = data => ({
 	data
 });
 
-export const fetchPrice = symbol => dispatch => (
+export const fetchPrice = symbol => (
 	APIUtil.fetchPrice(symbol)
-		.then(res => dispatch(receivePrice(res.data["Global Quote"])))
+		.then(res => {
+			// console.log(res.data["Global Quote"]);
+			if (res.data["Global Quote"] !== undefined) {
+				return res.data["Global Quote"]["05. price"];
+			} else {
+				return "";
+			}
+		})
 	//.then(res => console.log(res.data["Global Quote"]["01. symbol"]))
 );
