170753744bf1c2c461aed954459abf2563ba977d
Abhi Gupta <abhifgupta@gmail.com>
2019-10-30 15:47:01 -0700

    inital heroku deployment

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const mongoose = require('mongoose');
-// creates express server
+const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
+const path = require("path");
+// parse JSON
+const bodyParser = require("body-parser");
+// authenticate web tokens for user auth
+const passport = require("passport");
 // imports mongo cluster key
-const db = require('./config/keys').mongoURI;
-// imports routes from "<url>"
-const users = require("./routes/api/users");
+const db = require("./config/keys").mongoURI;
+
+// import routes here
 const accounts = require("./routes/api/accounts");
 const assets = require("./routes/api/assets");
 const holdings = require("./routes/api/holdings");
-// parse JSON
-const bodyParser = require('body-parser');
-// authenticate web tokens for user auth
-const passport = require('passport');
+const users = require("./routes/api/users");
+
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('frontend/build'));
+	app.get('/', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+	});
+}
 
 mongoose
 .connect(db, { useNewUrlParser: true })
@@ -24,12 +33,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(passport.initialize());
-require('./config/passport')(passport);
+require("./config/passport")(passport);
+
 // initial routes here, tries to match first arg, then will send the objects we are importing above from the routes/api folder
-app.use("/api/users", users);
 app.use("/api/accounts", accounts);
 app.use("/api/assets", assets);
 app.use("/api/holdings", holdings);
+app.use("/api/users", users);
+
 // app will run on Heroku || localhost:5000
 const port = process.env.PORT || 5000;
 
