1d728d8cad3c59f2f5d86193c209747be4188b26
Abhi Gupta <abhifgupta@gmail.com>
2020-01-03 15:12:29 -0800

    create new ajax requests to get all price symbols from db, fetch prices, and update db with latest price -- process kicked off when the Splash page renders

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {updatePrices} from '../../util/price_api_util';
 
 class Splash extends React.Component {
 
 	render() {
+		updatePrices();
 		return (
 			<div className="hero-banner">
 				<div className="hero-image">
--- frontend/src/util/price_api_util.js
+++ frontend/src/util/price_api_util.js
@@ -3,3 +3,30 @@ import axios from 'axios';
 export const fetchPrices = symbol => {
 	return axios.get(`api/prices/${symbol}`);
 };
+
+export const updatePrice = symbol => {
+	return axios.put(`api/prices/`);
+};
+
+export const updatePrices = () => {
+	axios.get(`api/prices/`)
+		.then(res => {
+			// get all the stock symbols from the Price collection, store as an array in symbols below
+			const symbols = res.data;
+
+			symbols.forEach(symbol => {
+				fetchPrices(symbol)
+					.then(res => {
+						const price = res.data.latestPrice;
+						axios.put(`api/prices/`, [symbol, price]);
+					})
+					.catch(err => {
+						fetchPrices(symbol)
+							.then(res => {
+								const price = res.data.latestPrice;
+								axios.put(`api/prices/`, [symbol, price]);
+							});
+					});
+			});
+		});
+};
